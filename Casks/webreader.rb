0463ef7b3d1900b53f43cc0b6424b40d78a47381
Jesper Pedersen <pejes@aarhus.dk>
2026-09-21 14:59:41 +0200

    Update webreader cask to 0.15.0

--- Casks/webreader.rb
+++ Casks/webreader.rb
@@ -2,10 +2,10 @@ cask "webreader" do
   version "0.15.0"
   sha256 "3d7634f9426df40910a6f636bb0067566fd6c3a8b8b20a29fe5ccab20ffc3960"
 
-  url "https://github.com/yepzdk/webreader/releases/download/v#{version}/WebReader-#{version}.zip"
+  url "https://webreader.yepz.dk/download/WebReader-#{version}.zip"
   name "WebReader"
   desc "Distraction-free reading app: send it a link, read the article"
-  homepage "https://github.com/yepzdk/webreader"
+  homepage "https://webreader.yepz.dk/"
 
   depends_on macos: :ventura
 
